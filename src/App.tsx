fbe3770c15e7c3263b74ebfa40a0087ea15ed140
emsthf <deunsol916@gmail.com>
2022-01-31 11:45:30 +0900

    exercise selector get, set

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinuteChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
+    </div>
+  );
+}
+
+export default App;
